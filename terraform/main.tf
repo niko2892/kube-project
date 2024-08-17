1eb481ac4ce19287b4ab4400c0b0d706dc8173b9
unknown <U_M18AL@HNBA105838.alfaextra.net>
2024-08-17 12:44:09 +0300

    change folder creations script

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+variable "cloud_catalog_title" {
+  type = string
+  description = "Название каталога"
+}
+
+variable "token" {
+  type = string
+  description = "токен"
+}
+
+variable "cloud_id" {
+  type = string
+  description = "id каталога"
+}
+
+variable "zone" {
+  type = string
+  description = "зона доступности"
+}
+
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token ="${var.token}"
+  zone = "${var.zone}"
+}
+
+#создание каталога в облаке
+resource "yandex_resourcemanager_folder" "app-folder" {
+    cloud_id    = "${var.cloud_id}"
+    name        = "${var.cloud_catalog_title}"
+}
--- terraform/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-token ="token"
-zone="ru-central1-a"
-cloud_id="id"
-cloud_catalog_title="kuber"
