5149c1a87660ad3d8e224c532010793af76a3dfa
unknown <U_M18AL@HNBA105838.alfaextra.net>
2024-10-24 20:49:53 +0300

    add dmz

---
+++ terraform/run-yc-managed-kuber/dns-zone.tf
@@ -0,0 +1,11 @@
+resource "yandex_dns_zone" "zone1" {
+  name        = "kuber-dns"
+  description = "kuber-dns"
+  folder_id = var.folder_id
+  labels = {
+    label1 = "kuber-dns"
+  }
+
+  zone    = "kuber."
+  public  = true
+}
