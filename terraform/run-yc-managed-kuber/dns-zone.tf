cc7e25d00b9205857ddf446cec97f88f82c34650
unknown <U_M18AL@HNBA105838.alfaextra.net>
2024-10-27 20:50:55 +0300

    sa

--- terraform/run-yc-managed-kuber/dns-zone.tf
+++ terraform/run-yc-managed-kuber/dns-zone.tf
@@ -1,11 +1,11 @@
-# resource "yandex_dns_zone" "zone1" {
-#   name        = "kuber-dns"
-#   description = "kuber-dns"
-#   folder_id = var.folder_id
-#   labels = {
-#     label1 = "kuber-dns"
-#   }
+resource "yandex_dns_zone" "zone1" {
+  name        = "kuber-dns"
+  description = "kuber-dns"
+  folder_id = var.folder_id
+  labels = {
+    label1 = "kuber-dns"
+  }
 
-#   zone    = "studygroup.tech."
-#   public  = true
-# }
+  zone    = "studygroup.tech."
+  public  = true
+}
